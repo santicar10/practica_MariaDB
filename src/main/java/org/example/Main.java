2616c2086dd5e5223743154b845a10d06c3b3420
StevenSantoss <107423300+StevenSantoss@users.noreply.github.com>
2023-03-27 08:58:58 -0500

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/tiendecita";
+        String user = "root";
+        String password = "admin";
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+             Statement statement = conn.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT * FROM products")
+             ) {
+            while (resultSet.next()) {
+                System.out.print(resultSet.getInt("id"));
+                System.out.print("|");
+                System.out.print(resultSet.getString("nombre"));
+                System.out.print("|");
+                System.out.print(resultSet.getDouble("precio"));
+                System.out.print("|");
+                System.out.print(resultSet.getDate("fecha_registro"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
