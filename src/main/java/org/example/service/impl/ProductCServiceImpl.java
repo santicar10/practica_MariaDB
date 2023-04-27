6fc97d3c8bd775f508a5c15c5d645d186d9f1df0
StevenSantoss <107423300+StevenSantoss@users.noreply.github.com>
2023-04-27 00:34:25 -0500

    FINISH

---
+++ src/main/java/org/example/service/impl/ProductCServiceImpl.java
@@ -0,0 +1,43 @@
+package org.example.service.impl;
+
+import org.example.model.Product;
+import org.example.model.ProductCategory;
+import org.example.repository.impl.ProductCRepostoryImp;
+import org.example.service.ProductService;
+
+import java.util.ArrayList;
+
+public class ProductCServiceImpl implements ProductService {
+
+    ProductCRepostoryImp repository=new ProductCRepostoryImp();
+    @Override
+    public void getlist() {
+        System.out.println("------------LIST PRODUCTS------------");
+        ArrayList<ProductCategory> products=(ArrayList<ProductCategory>) repository.list();
+        System.out.println(products);
+    }
+
+    @Override
+    public void getbyId(Long id) {
+        System.out.println("------------GET PRODUCT BY ID------------");
+        System.out.println(repository.byId(id));
+    }
+
+    @Override
+    public void save(Object o) {
+        System.out.println("------------SAVE PRODUCT------------");
+        repository.save(o);
+    }
+
+    @Override
+    public void delete(Long id) {
+        System.out.println("------------DELETE PRODUCT------------");
+        repository.delete(id);
+    }
+
+    @Override
+    public void update(Long id,Object o) {
+        System.out.println("------------UPDATE PRODUCT------------");
+        repository.update(id,o);
+    }
+}
